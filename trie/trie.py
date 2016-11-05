d42765f4ad54c1680c7b1cc908feee7b6d11b905
oisdk <oisin.kidney@gmail.com>
2016-11-05 17:40:53 +0000

    more examples

--- trie/trie.py
+++ trie/trie.py
@@ -12,12 +12,12 @@ class Trie:
     def member(self, word):
         """
         Returns a bool representing whether or not the sequence
-        `word` is in the trie `self`
+        `word` is in the trie `self`.
 
         >>> t = Trie()
         >>> t.member("abc")
         False
-        >>> t.insert("abc")
+        >>> t.add("abc")
         >>> t.member("abc")
         True
         """
@@ -29,16 +29,16 @@ class Trie:
                 return False
         return current._endsHere
 
-    def insert(self, word):
+    def add(self, word):
         """
-        inserts the sequence `word` into `self`
+        adds the sequence `word` into `self`.
 
         >>> t = Trie()
         >>> print(t)
         []
-        >>> t.insert("abc")
+        >>> t.add("abc")
         >>> print(t)
-        [['a', 'b', 'c']]
+        ['abc']
         """
         current = self
         for letter in word:
@@ -46,35 +46,170 @@ class Trie:
         current._endsHere = True
 
 ########################################################################
+# That's all that was covered on Saturday, below here is stuff you
+# might want to try implementing yourself before you have a look at it.
+# If you're stuck, though, feel free to use it!
+
+    def suffixes(self, word):
+        """
+        Returns a trie of the suffixes of `word` in `self`.
+
+        >>> t = Trie.fromWords("carpet", "carpark")
+        >>> t.suffixes("car")
+        ['park', 'pet']
+        """
+        result = self
+        for letter in word:
+            try:
+                result = result._children[letter]
+            except KeyError:
+                return Trie()
+        return result
+
+    def remove(self, word):
+        self._remove(iter(word))
+
+    def _remove(self, word_iter):
+        """Internal method for removing a word from the trie. If the
+        word isn't present, it will raise a KeyError.
+
+        It also returns a bool, to optimize space. Consider, for
+        instance, the trie of ["car", "carpet"]:
+
+        c-a-r.p-e-t.
+
+        To delete "car", the tag on 'r' can just be flipped:
+
+        c-a-r-p-e-t.
+
+        To delete "carpet" and keep "car", the tag on 't' must be
+        flipped:
+
+        c-a-r.p-e-t
+
+        But now the p-e-t is being stored unnecessarily.
+
+        For this reason, the function returns a bool, which means
+        "delete me!" when True, or "keep me!" when False.
+        """
+        try:
+            head = next(word_iter)
+            chld = self._children[head]
+            if chld._remove(word_iter):
+                del self._children[head]
+        except StopIteration:
+            self._endsHere = False
+        return not (self._endsHere or self._children)
+
+    def __len__(self):
+        return sum(map(len, self._children.values())) + self._endsHere
+
+
+########################################################################
 # Below here is stuff not necessary for the algorithm, just useful for
 # debugging, etc.
+
+    def discard(self, word):
+        try: self.remove(word)
+        except KeyError: pass
+
+    def __contains__(self, word):
+        return self.member(word)
 
     def fromWords(*words):
         """
         Creates a trie from several words.
 
         >>> Trie.fromWords("abc", "def")
-        [['a', 'b', 'c'], ['d', 'e', 'f']]
+        ['abc', 'def']
         >>> Trie.fromWords([1,2,3],[4,5,6])
         [[1, 2, 3], [4, 5, 6]]
         """
         trie = Trie()
         for word in words:
-            trie.insert(word)
+            trie.add(word)
         return trie
 
     def _values(self, prefix):
         """Helper function to allow iteration"""
         if self._endsHere:
             yield prefix
+        is_str = type(prefix) is str
         for head, child in sorted(self._children.items()):
-            yield from child._values(prefix + [head])
+            if is_str and type(head) is str:
+                next_prefix = prefix + head
+            else:
+                next_prefix = list(prefix) + [head]
+            yield from child._values(next_prefix)
+
+    def __eq__(self, other):
+        return self._endsHere == other._endsHere and self._children == other._children
 
     def __iter__(self):
-        return self._values([])
+        return self._values("")
 
     def __repr__(self):
         return list(self).__repr__()
 
     def __str__(self):
         return str(list(self))
+
+    def _debug_view(self, size, pad, first=False):
+
+
+        if first:
+            wspaces = [('','')] * len(self._children)
+            npad = ''
+        else:
+            yield '.' if self._endsHere else '─'
+            npad = pad + '{0:>{width}}'.format('│ ', width=size+1)
+            wspaces  = [('',npad)]
+            wspaces += [(pad + '{0:>{width}}'.format('├─', width=size+1),npad)
+                        for _ in range(len(self._children)-2)]
+            if len(self._children) > 1:
+                wspaces += [(pad + '{0:>{width}}'.format('└─', width=size+1),
+                             npad)]
+            wspaces[-1] = (wspaces[-1][0], pad + (' ' * (size+1)))
+        it_by_len = sorted(self._children.items())
+        for (wspace,npad), (head, child) in zip(wspaces, it_by_len):
+            yield wspace
+            yield '{0:>{width}}'.format(head, width=size)
+            yield from child._debug_view(size, npad)
+        if not self._children:
+            yield '\n'
+
+    def letters(self):
+        for head, child in self._children.items():
+            yield head
+            yield from child.letters()
+
+    def debug_view(self):
+        s = max((len(str(l)) for l in self.letters()), default=0)
+        view = self._debug_view(s, 0, True)
+        return ''.join(view)
+
+def complete():
+    import re
+    with open('sherlock_holmes.txt') as file:
+        trie = Trie()
+        for line in file:
+            for word in re.split('\W+', line):
+                trie.add(word.lower())
+        print('type a word to see its suffixes. q to quit.')
+        for line in iter(input, 'q'):
+            word = line.strip().lower()
+            for suff in trie.suffixes(word):
+                print(word + suff)
+            print()
+
+def build():
+    trie = Trie()
+    print('type in a word to add it to the trie. q to quit.')
+    for line in iter(input, 'q'):
+        print()
+        trie.add(line.strip().lower())
+        print(trie.debug_view())
+
+if __name__ == '__main__':
+    complete()
+    build()
--- trie/trie_tests.py
+++ trie/trie_tests.py
@@ -1,43 +1,64 @@
 from trie import *
 import unittest
-from random import randrange
+from random import randrange, choice
+import re
 
 
-def rand_tuple(length):
-    return tuple(randrange(30) for _ in range(30))
 
 
-def rand_tuples(length):
-    return [rand_tuple(randrange(10)) for _ in range(length)]
+with open('sherlock_holmes.txt') as file:
+    words = set()
+    for line in file:
+        for word in re.split('\W+', line):
+            words.add(word.strip().lower())
+    words = list(words)
+
+def rand_word(length):
+    return choice(words)
+
+
+def rand_words(length):
+    return [rand_word(randrange(10)) for _ in range(length)]
 
 
 class TrieTests(unittest.TestCase):
     def testInsertIsMember(self):
         for _ in range(100):
             t = Trie()
-            words = rand_tuples(randrange(30))
+            words = rand_words(randrange(30))
             for word in words:
-                t.insert(word)
+                t.add(word)
             for word in words:
                 self.assertTrue(t.member(word))
 
     def testMakeTrie(self):
         for _ in range(100):
-            words = rand_tuples(randrange(30))
+            words = rand_words(randrange(30))
             t = Trie.fromWords(*words)
             for word in words:
                 self.assertTrue(t.member(word))
 
     def testIter(self):
         for _ in range(100):
-            words = rand_tuples(randrange(30))
+            words = rand_words(randrange(30))
             t = Trie.fromWords(*words)
-            l = sorted(map(list, set(words)))
+            l = [''.join(w) for w in sorted(set(words))]
             self.assertEqual(list(t),l)
+
+    def testRemove(self):
+        for _ in range(100):
+            words = rand_words(randrange(30))
+            word = rand_word(randrange(15))
+            if not (word in words):
+                t = Trie.fromWords(*words)
+                tr = Trie.fromWords(*words)
+                tr.add(word)
+                tr.remove(word)
+                self.assertEqual(t,tr)
 
 
 if __name__ == '__main__':
-    unittest.main()
     import doctest
     import trie
     doctest.testmod(m=trie)
+    unittest.main()
